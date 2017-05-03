a1c7c1b8aa3a99cba195e17de53f77da12e924d5
Letícia Santos <le.sisantos@gmail.com>
2017-05-02 22:17:10 -0300

    Aula 11 list e repetição

---
+++ AulaArrayandList/AulaArrayandList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lista = new List<int>();
+            lista.Add(1);
+        }
+    }
+}
