590b7e103c49f3c7c7f9107465b042ff08e0bfc9
Letícia Santos <le.sisantos@gmail.com>
2017-05-10 21:31:47 -0300

    Struct

---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+
+        public struct Livro
+        {
+            public int codigo;
+            public string nomeAutor;
+            public string titulo;
+        }
+        static void Main(string[] args)
+        {
+            Livro livro1;
+            livro1.codigo = 1;
+            livro1.nomeAutor = "Teste";
+            livro1.titulo = "Livro do teste";
+        }
+    }
+}
