44f0a68cfc5e83dff924515125f4f518c46cb447
Letícia Santos <le.sisantos@gmail.com>
2017-05-10 21:46:25 -0300

    Foreach

--- Struct/Struct/Program.cs
+++ Struct/Struct/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,6 +22,22 @@ namespace Struct
             livro1.codigo = 1;
             livro1.nomeAutor = "Teste";
             livro1.titulo = "Livro do teste";
+
+            Livro livro2;
+            livro2.codigo = 2;
+            livro2.nomeAutor = "Teste2";
+            livro2.titulo = "Livro do teste2";
+
+            ArrayList livros = new ArrayList();
+            livros.Add(livro1);
+            livros.Add(livro2);
+
+            foreach (Livro l in livros)
+            {
+                Console.WriteLine("Codigo:" + l.codigo.ToString());
+                Console.WriteLine("Codigo:" + l.nomeAutor);
+            }
+            Console.ReadKey();
         }
     }
 }
