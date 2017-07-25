0797c5565be91c5bc734a8bc648f8ef77c2108b4
Letícia Santos <le.sisantos@gmail.com>
2017-07-24 22:16:29 -0300

    lss criação de classes

---
+++ Conceitos/Nota/CadernoNota.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nota
+{
+    class CadernoNota
+    {
+        //ctor - criação de um construtor
+        public CadernoNota()
+        {
+
+        }
+
+        List<float> notas = new List<float>();
+        public void AddNota(float nota)
+        {
+            notas.Add(nota);
+        }
+    }
+}
---
+++ Conceitos/Nota/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nota
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CadernoNota caderno = new CadernoNota();
+
+            caderno.AddNota(1);
+            caderno.AddNota(2.3f);
+        }
+    }//
+}
