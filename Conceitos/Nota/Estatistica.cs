f1e9fece8b297d590c93fa7fe322b0c7fa27dfb0
Letícia Santos <le.sisantos@gmail.com>
2017-10-05 22:32:42 -0300

    Events

--- Conceitos/Nota/Estatistica.cs
+++ Conceitos/Nota/Estatistica.cs
@@ -14,6 +14,60 @@ namespace Nota
             LowestGrade = float.MaxValue;
         }
 
+        public string LetterGrade
+        {
+            get
+            {
+                string result;
+                if (AvarageGrade >= 90)
+                {
+                    result= "A";
+                }
+                else if(AvarageGrade >= 80) {
+                    result= "B";
+                }
+                else if (AvarageGrade >=70)
+                {
+                    result = "C";
+                } else if (AvarageGrade >= 60)
+                {
+                    result = "D";
+                }
+                else
+                {
+                    result = "F";
+                }
+                return result;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                string result;
+                switch (LetterGrade)
+                {
+                    case "A":
+                        result = "Excellent";
+                        break;
+                    case "B":
+                        result = "Good";
+                        break;
+                    case "C":
+                        result = "Average";
+                        break;
+                    case "D":
+                        result = "Below Average";
+                        break;
+                    default:
+                        result = "Failing";
+                        break;
+                }
+                return result;
+            }
+        }
+
         public float AvarageGrade;
         public float HighestGrade;
         public float LowestGrade;
--- Conceitos/Nota/Program.cs
+++ Conceitos/Nota/Program.cs
@@ -12,46 +12,54 @@ namespace Nota
         {
             CadernoNota caderno = new CadernoNota();
 
-            caderno.NameChanged += new NameChangedDelegate(OnNameChanged);
+            /*caderno.NameChanged += new NameChangedDelegate(OnNameChanged);
             //caderno.NameChanged += new NameChangedDelegate(OnNameChanged2);
             //caderno.NameChanged += OnNameChanged2;
 
             caderno.Nome = "Notas Aluno";
-            caderno.Nome = "Caderno teste";
+            caderno.Nome = "Caderno teste";*/
 
             caderno.AddNota(91);
             caderno.AddNota(89.5f);
             caderno.AddNota(75);
 
             Estatistica estatis = caderno.CalcularEstatistica();
-            Console.WriteLine(caderno.Nome);
+            //Console.WriteLine(caderno.Nome);
 
             WriteResult("Avarage", estatis.AvarageGrade);
-            WriteResult("Highest", (int)estatis.HighestGrade);
+            WriteResult("Highest", estatis.HighestGrade);
             WriteResult("Lowest", estatis.LowestGrade);
+            WriteResult("Grade", estatis.LetterGrade);
+            WriteResult(estatis.Description, estatis.LetterGrade);
             Console.ReadKey();
 
 
         }
 
-        static void OnNameChanged(object sender, NameChangedEventArgs args)
+       /* static void OnNameChanged(object sender, NameChangedEventArgs args)
         {
             Console.WriteLine($"Estamos trocando o nome de {args.ExistingName} para {args.NewName} !" );
-        }
+        }*/
 
        /*( static void OnNameChanged2(String existente, string novo)
         {
             Console.WriteLine("***");
         }*/
 
-        static void WriteResult(string descricao, int resultad)
-        {
-            Console.WriteLine($"{descricao} : {resultad}");
-        }
-
         static void WriteResult(string descricao, float resultad)
         {
-            Console.WriteLine($"{descricao} : {resultad}");
+            Console.WriteLine($"{descricao} : {resultad:f2}", descricao,resultad);
         }
+
+        static void WriteResult(string descricao, string resultad)
+        {
+            Console.WriteLine($"{descricao} : {resultad}", descricao, resultad);
+        }
+
+
+        /*static void WriteResult(string descricao, float resultad)
+        {
+            Console.WriteLine($"{descricao} : {resultad}");
+        }*/
     }
 }
